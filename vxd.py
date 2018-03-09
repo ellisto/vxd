446ec0d0d3f72e1a8488aac910816f1a2761caad
tje <thomasjayellis@gmail.com>
2018-03-09 11:39:14 -0500

    replace hexdump with custom function
     - also add status line on opening file that gets cleared on first key
     - add swp to gitignore
     - remove swp that accidentally was committed

--- vxd.py
+++ vxd.py
@@ -2,7 +2,6 @@
 
 import curses
 import sys
-from hexdump import hexdump
 from blessed import Terminal
 import functools
 
@@ -13,17 +12,21 @@ class vxd():
         self.file = file
         self.statusline = "Opening {}...".format(self.file)
         self.term = Terminal()
+        self.buf = None
+        self.redraw()
         with open(file, 'rb') as f:
-            self.buf = f.read(400)
+            self.buf = f.read()
+            self.statusline += 'read {} bytes'.format(len(self.buf))
+        if self.buf is not None:
+            self.selected_byte = 0
+        self.redraw()
 
     def clear(self):
         echo(self.term.clear)
 
     def redraw_status(self):
          with self.term.location(0, self.term.height):
-             self.term.clear_eos()
-             print(self.statusline)
-
+             echo(self.statusline)
 
     def redraw(self):
         self.clear()
@@ -31,8 +34,34 @@ class vxd():
         self.printbuf()
 
     def printbuf(self):
+        if self.buf is None:
+            return
+        bpl = 16
+        height = self.term.height - 1
+        numbytes = bpl * height
+        asc_line = []
+        bnum = 0
         with self.term.location(0,0):
-            print(hexdump(self.buf, 'return'))
+            for i, b in enumerate(self.buf):
+                if i == numbytes:
+                    #bnum = i % bpl
+                    #if bnum > 0:
+                    #    echo(' ' * ((bpl-bnum) * 3 + 1) + ''.join(asc_line))
+                    break
+                bnum = i % bpl
+                if bnum == 0:
+                    echo('{:08x} '.format(i))
+                echo('{:02x} '.format(b))
+                #todo make nonprintables distinct from a literal '.' (maybe dim formatting?)
+                asc_line.append(chr(b) if (b > 0x1f and b < 0x7f) else '.') 
+                if bnum == bpl - 1: # time for new line
+                    echo(' {}'.format(''.join(asc_line)))
+                    asc_line.clear()
+                    echo('\r\n')
+            if len(asc_line) > 0:
+                echo(' ' * ((bpl-bnum) * 3 + 1) + ''.join(asc_line))
+
+            #echo(hexdump(self.buf[:400], 'return'))
         
 
     def bmain(self):
@@ -46,11 +75,11 @@ class vxd():
             inp = None
             while True:
                 inp = term.inkey()
+                self.statusline = ''
+                self.redraw()
                 # q quits
                 if inp == 'q':
                     break
-                elif inp == 'r':
-                    self.redraw()
         self.clear()
 
 if __name__ == '__main__':
