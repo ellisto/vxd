e46663f9a9e65bbe16c9eec9ff5432a23f3ed829
tje <thomasjayellis@gmail.com>
2018-03-09 11:55:19 -0500

    add rudimentary hjkl selection of byte

--- vxd.py
+++ vxd.py
@@ -8,8 +8,9 @@ import functools
 echo = functools.partial(print, end='', flush=True)
 
 class vxd():
-    def __init__(self, file=''):
+    def __init__(self, file='', bpl=16):
         self.file = file
+        self.bpl = bpl
         self.statusline = "Opening {}...".format(self.file)
         self.term = Terminal()
         self.buf = None
@@ -40,11 +41,12 @@ class vxd():
     def printbuf(self):
         if self.buf is None:
             return
-        bpl = 16
+        bpl = self.bpl
         height = self.term.height - 1
         numbytes = bpl * height
         asc_line = []
         bnum = 0
+        t = self.term
         with self.term.location(0,0):
             for i, b in enumerate(self.buf):
                 if i == numbytes:
@@ -52,7 +54,7 @@ class vxd():
                 bnum = i % bpl
                 if bnum == 0:
                     echo('{:08x} '.format(i))
-                echo('{:02x} '.format(b))
+                echo('{}{:02x}{} '.format(t.standout if i == self.selected_byte else t.no_standout,  b, t.no_standout))
                 #todo make nonprintables distinct from a literal '.' (maybe dim formatting?)
                 asc_line.append(chr(b) if (b > 0x1f and b < 0x7f) else '.') 
                 if bnum == bpl - 1: # time for new line
@@ -75,10 +77,18 @@ class vxd():
             while True:
                 inp = term.inkey()
                 self.statusline = ''
-                self.redraw()
                 # q quits
                 if inp == 'q':
                     break
+                elif inp == 'h':
+                    self.selected_byte -= 1 if self.selected_byte > 0 else 0
+                elif inp == 'l':
+                    self.selected_byte += 1 if self.selected_byte < len(self.buf) else 0
+                elif inp == 'j':
+                    self.selected_byte += self.bpl if (self.selected_byte + self.bpl < len(self.buf)) else 0
+                elif inp == 'k':
+                    self.selected_byte -= self.bpl if (self.selected_byte - self.bpl > 0 ) else 0
+                self.redraw()
         self.clear()
 
 
