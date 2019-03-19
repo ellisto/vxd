0402b11410da90be7490dfc5f8caa5e82a3ac174
tje <thomasjayellis@gmail.com>
2019-03-19 11:10:13 -0400

    fix status line display in diff mode
     - also add optional debug log

--- vxd.py
+++ vxd.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
+import time
 from blessed import Terminal
 import functools
 
 echo = functools.partial(print, end='', flush=True)
 
 class vxd():
-    def __init__(self, filepath='', filepath2=None, bpl=16):
+    def __init__(self, filepath='', filepath2=None, bpl=16, debug_log=False):
+        self.debug_log = debug_log
         self.file = filepath
         self.file2 = filepath2
         self.bpl = bpl
@@ -36,6 +38,11 @@ class vxd():
         self.redraw()
 
 
+    def debug(self, msg):
+        if self.debug_log:
+            with open('log.txt', 'a') as f:
+                f.write("[{}] {}\n".format(time.asctime(), msg))
+
     def clear(self):
         ''' clear screen '''
         echo(self.term.clear)
@@ -43,9 +50,13 @@ class vxd():
 
     def redraw_status(self):
         ''' redraw both statuslines '''
-        self.print_statusline(self.statusline, self.term.height, self.file)
+
+        self.print_statusline(self.statusline,
+                              self.term.height if self.statusline2 is None
+                              else self.second_buf_start() - 1, self.file)
+
         if self.statusline2 is not None:
-            self.print_statusline(self.statusline2, self.last_displayed_row() + 1, self.file2)
+            self.print_statusline(self.statusline2, self.term.height, self.file2)
 
     def print_statusline(self, statusline, row, filename=None):
         '''print statusline at specified row.
@@ -64,13 +75,17 @@ class vxd():
         self.redraw_status()
         self.printbuf(self.buf, self.buf2)
         if self.buf2:
-            row_offset = self.last_displayed_row() + 2
+            row_offset = self.second_buf_start()
             self.printbuf(self.buf2, self.buf, row_offset)
 
+    def second_buf_start(self):
+        return (self.term.height // 2)
 
     def last_displayed_byte(self):
         ''' return the index of the last byte displayed onscreen '''
-        return self.first_displayed_byte + self.num_bytes_displayed() - 1
+        val = self.first_displayed_byte + self.num_bytes_displayed() - 1
+        self.debug("last_displayed_byte = {} + {} - 1 = {:x}".format(self.first_displayed_byte, self.num_bytes_displayed(), val))
+        return val
 
     def last_byte(self):
         ''' return the index of the last byte in the active buffer'''
@@ -78,7 +93,9 @@ class vxd():
 
     def last_displayed_row(self):
         ''' return the index of the row containing the last displayed byte'''
-        return self.row_of(self.last_displayed_byte())
+        val = self.row_of(self.last_displayed_byte())
+        self.debug("last_displayed_row: {}".format(val))
+        return val
 
     def num_bytes_displayed(self):
         ''' return the number of bytes displayed from a single buf on the
@@ -88,7 +105,8 @@ class vxd():
         if self.file2:
             height -= 1 # leave room for a second filename line
             height //= 2 # second file takes half the screen
-        return self.bpl * height
+        val = self.bpl * height
+        return val
 
 
     def row_of(self, byteidx):
@@ -108,7 +126,7 @@ class vxd():
         asc_line = []
         numbytes = self.num_bytes_displayed()
         first_displayed_row = self.row_of(self.first_displayed_byte)
-        last_displayed_row = self.row_of(self.last_displayed_byte())
+        last_displayed_row = self.last_displayed_row()
         selected_row = self.row_of(self.selected_byte)
         if selected_row > last_displayed_row:
             rows_to_scroll = selected_row - last_displayed_row
@@ -117,9 +135,12 @@ class vxd():
             rows_to_scroll = first_displayed_row - selected_row
             self.first_displayed_byte -= bpl * rows_to_scroll
 
+
         offset = self.first_displayed_byte
         bnum = 0
         t = self.term
+        self.debug("offset: {}".format(row_offset))
+        self.debug("row_offset: {}".format(row_offset))
         with self.term.location(0, row_offset):
             for i, b in enumerate(buf[offset:]):
                 if i == numbytes:
@@ -128,7 +149,7 @@ class vxd():
                 if bnum == 0:
                     echo(t.blue('{:08x} '.format(i + offset)))
 
-                active = (i + offset == self.selected_byte) 
+                active = (i + offset == self.selected_byte)
                 h = '{:02x}'.format(b)
                 sep = '  ' if bnum == bpl//2 - 1 else ' '
                 char = t.bold(chr(b)) if (b > 0x1f and b < 0x7f) else t.dim('.')
@@ -137,7 +158,7 @@ class vxd():
                         char = t.red(char)
                         h = t.red(h)
                 echo('{}{}'.format(t.standout(h) if active else h, sep))
-                asc_line.append(t.standout(char) if active else char) 
+                asc_line.append(t.standout(char) if active else char)
                 if bnum == bpl - 1: # time for new line
                     echo(' {}'.format(''.join(asc_line)))
                     asc_line.clear()
@@ -171,10 +192,10 @@ class vxd():
                 elif inp == 'l' or inp.code  == t.KEY_RIGHT:
                     self.selected_byte += 1 if self.selected_byte < self.last_byte() else 0
                 elif inp == 'j' or inp.code  == t.KEY_DOWN:
-                    self.selected_byte += self.bpl if (self.selected_byte + self.bpl < self.last_byte()) else 0
+                    self.selected_byte += self.bpl if (self.selected_byte + self.bpl <= self.last_byte()) else 0
                 elif inp == 'k' or inp.code  == t.KEY_UP:
                     self.selected_byte -= self.bpl if (self.selected_byte - self.bpl >= 0 ) else 0
-                # g or home go to beginning 
+                # g or home go to beginning
                 elif inp == 'g' or inp.code  == t.KEY_HOME:
                     self.selected_byte = 0
                 # G or end go to end
